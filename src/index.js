d2163859aeee6916c1ee527f1eedf3a52da80712
Ishita <95162790+ishita-kundi04@users.noreply.github.com>
2022-03-19 16:28:34 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './statewise.css';
+import Statewise from "./Statewise";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Statewise />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
